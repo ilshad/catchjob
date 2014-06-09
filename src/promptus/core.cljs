cf7d26b7b444cf71f0f1e72fef1a1a6967e9f135
Ilshad Khabibullin <astoon@spacta.com>
2014-06-09 05:39:22 +0400

    client app structure

--- src/promptus/core.cljs
+++ src/promptus/core.cljs
@@ -2,13 +2,71 @@
   (:require-macros [cljs.core.async.macros :refer [go-loop]])
   (:require [cljs.core.async :refer [put! chan <! >!]]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [promptus.util :as util :refer [div ul li span icon]]))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:entries [{:description "foo bar"}
+                                {:description "aua"}]
+                      :messages ["foo" "bar"]}))
 
-(defn root [app owner]
-  (om/component (dom/h1 nil (:text app))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Topbar
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(om/root root app-state {:target (. js/document (getElementById "app"))})
+(defn menu-item [on-click active? & contents]
+  (li (when active? "active")
+    (apply dom/a
+           #js {:href "#"
+                :onClick on-click}
+           contents)))
+
+(defn topbar [app _]
+  (div "header"
+    (ul "nav nav-pills pull-right"
+      (menu-item (constantly nil) false "Post a job")
+      (menu-item (constantly nil) true "Messages"
+                 (let [c (-> app :messages count)]
+                   (when (> c 0)
+                     (span "badge" (str c))))))
+    (dom/h2
+     #js {:className "text-muted"}
+     "prompt/us")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Wall
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn entry-view [entry _]
+  (div "well well-sm" (:description entry)))
+
+(defn wall [app _]
+  (om/component
+   (apply div "wall"
+          (om/build-all entry-view (:entries app)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Layout
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn root [app _]
+  (om/component
+   (div "container"
+     (om/build topbar app)
+     (om/build wall app))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Main
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(om/root root app-state
+         {:target (. js/document (getElementById "app"))})
---
+++ src/promptus/util.cljs
@@ -0,0 +1,19 @@
+(ns promptus.util
+  (:require [om.dom :as dom :include-macros true]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; DOM shorthands
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn tag [tag class-names & contents]
+  (apply tag #js {:className class-names} contents))
+
+(def div   (partial tag dom/div))
+(def ul    (partial tag dom/ul))
+(def li    (partial tag dom/li))
+(def span  (partial tag dom/span))
+
+(defn icon [& class-names]
+  (dom/i #js {:className (apply str (interpose " " (conj class-names "fa")))}))
